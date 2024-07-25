71559b24373d84c97e2fff8f2413419bf3c1e499
codecraftmans <158328545+codecraftmans@users.noreply.github.com>
2024-07-25 10:19:55 +0000

    code config

---
+++ basics/variable.js
@@ -0,0 +1,11 @@
+const employeId = 22323323;
+let employeName = "manish";
+var password = 2323444;
+// console.log(employeId);
+// console.log(employeName);
+// console.log(employeId);
+console.table([employeId,employeName,password]); //to print all the variables using this console.table
+// console.log(password , typeof password);
+// console.log(employeId , typeof employeId);
+employeId = 12342
+console.log(employeId); // const variable value cannot be changed because they have their some scope
