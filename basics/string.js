dcaa81579c75b95e659697c99e64211114de1b41
codecraftmans <158328545+codecraftmans@users.noreply.github.com>
2024-08-01 10:44:39 +0000

    assign the string code config

---
+++ basics/string.js
@@ -0,0 +1,18 @@
+// const gamename ="manishjoshi"
+// console.log(gamename.indexOf('j')); // if we want to find the index of a string we use indexof method
+
+// const another = "manishjoshi"
+// console.log(another.charAt(3)); // if we want to find the which index is given to the string we use charat method
+
+// const newgame = new String('manish') // create a object of newgame new is word
+// console.log(newgame);
+// console.log(newgame.toUpperCase()); //this is a function to capitalize all the string 
+// console.log(newgame.slice(-5,3));
+// console.log(newgame.indexOf('h'));
+
+const Studentname ="mansih";
+const score = 40;
+console.log(`helo my name is ${Studentname.toUpperCase()} my repo count is ${score}`); //this was a string interpoletaion and that was most 
+// readable or hum isme Koi bhi method Ko add Kr sKte hai
+console.log(Studentname.blink());
+
